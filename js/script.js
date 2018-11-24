94d1a91fdcc80511e19c940fffa8cf19e66770a8
Quang Vo <quangdvo@gmail.com>
2018-11-24 07:35:13 -0500

    Added Locations of Aircraft Incidents

--- js/script.js
+++ js/script.js
@@ -5,33 +5,6 @@ var regionHeight = 400;
 
 var selectedState = "";
 
-var admissionsButton = d3.select('#access-row')
-    .append('g')
-    .append('button')
-    .attr('id', 'admissions-btn')
-    .text('Admissions');
-var populationButton = d3.select('#access-row')
-    .append('g')
-    .append('button')
-    .attr('id', 'population-btn')
-    .text('Population');
-var financeButton = d3.select('#access-row')
-    .append('g')
-    .append('button')
-    .attr('id', 'finance-btn')
-    .text('Finance');
-var debtButton = d3.select('#access-row')
-    .append('g')
-    .append('button')
-    .attr('id', 'debt-btn')
-    .text('Debt');
-var employmentButton = d3.select('#access-row')
-    .append('g')
-    .append('button')
-    .attr('id', 'employment-btn')
-    .text('Employment');
-var selectedButton = "";
-
 // hidden hoverTooltip
 var hoverTooltip = d3.select('#vis').append('div')
     .attr('id', 'tooltip')
@@ -69,7 +42,8 @@ d3.queue()
             injuries: +d['Total Serious Injuries'],
             uninjured: +d['Total Uninjured'],
             weather: d['Weather Condition'],
-            flightPhase: d['Broad Phase of Flight']
+            flightPhase: d['Broad Phase of Flight'],
+            name: d['Location']
         };
     })
     .defer(d3.csv, 'csv/states.csv', function (d) {
@@ -86,8 +60,6 @@ function display(error, collegeCSV, stateCSV) {
     if (error) {
         console.log('Error importing data ' + error);
     }
-    var lastIndex = -1;
-    var activeIndex = 0;
 
     var mapSVG = d3.select('#vis')
         .append('svg')
@@ -95,12 +67,14 @@ function display(error, collegeCSV, stateCSV) {
         .attr('width', width)
         .attr('height', height);
 
-    var projection = d3.geoAlbersUsaPr(),
-        path = d3.geoPath().projection(projection);
-    var stateJson = topojson.feature(states, states.objects.states_20m_2017);
+    // draw map vis
+    var projection = d3.geoAlbersUsa().scale(1000).translate([width / 2, height / 2]);
+
+    var path = d3.geoPath().projection(projection);
+    var stateFeatures = topojson.feature(states, states.objects.states_20m_2017).features;
     // save states data onto d3 map features
-    for (var i = 0; i < stateJson.features.length; i++) {
-        var state = stateJson.features[i];
+    for (var i = 0; i < stateFeatures.length; i++) {
+        var state = stateFeatures[i];
         for (var j = 0; j < stateCSV.length; j++) {
             if (state.properties.GEOID == stateCSV[j].ansi) {
                 state.name = stateCSV[j].name;
@@ -108,17 +82,21 @@ function display(error, collegeCSV, stateCSV) {
             }
         }
     }
+    // filter US flights
+    var selection = collegeCSV.filter(function (d) {
+        return (d.country == "United States" && projection([d.longitude, d.latitude]));
+    });
 
     var locked = false;
 
     // state coloring method
     function recolorMap(selectedState) {
         mapSVG.selectAll('path')
-            .data(stateJson.features)
+            .data(stateFeatures)
             .classed('selected', false);
         if (error) throw error;
         mapSVG.selectAll('path')
-            .data(stateJson.features)
+            .data(stateFeatures)
             .filter(function (d) {
                 return d.name == selectedState;
             })
@@ -128,7 +106,7 @@ function display(error, collegeCSV, stateCSV) {
     // draw state path features
     var paths_states = mapSVG
         .append('g').selectAll('path')
-        .data(stateJson.features)
+        .data(stateFeatures)
         .enter().append('path')
         .attr('d', path)
         .classed('selected', function (d) {
@@ -152,35 +130,54 @@ function display(error, collegeCSV, stateCSV) {
                 locked = false;
             }
         })
-        .on('mousemove', function (d) {
-            var mouse = d3.mouse(mapSVG.node()).map(function (d) {
-                return parseInt(d);
-            });
-            hoverTooltip.classed('hidden', false)
-                .attr('style', 'left:' + (mouse[0] + 15) +
-                    'px; top:' + (mouse[1] - 35) + 'px')
-                .html(d.name + "<br />");
-            generateTooltip(d.name);
-        })
-        .on('mouseout', function () {
-            hoverTooltip.classed('hidden', true);
+        .on('mouseout', function (d) {
             if (!locked) {
                 recolorMap("");
             }
         });
 
-    admissionsButton.on('click', function () {
-        selectedButton = "admissions";
-        admissionsButton.classed('selected', true);
-    });
 
-    function generateTooltip(state) {
-        collegeCSV.filter(function (d) {
-            return d.name == selectedState;
+    var circles = mapSVG
+        .append('g')
+        .selectAll('circle')
+        .data(selection)
+        .enter()
+        .append('circle')
+        .attr('class', 'incident')
+        .attr('text', function (d) {
+            return d.location;
         })
-        d3.select('#tooltip')
-            .append('g')
+        .attr('cx', function (d) {
+            var pos = projection([d.longitude, d.latitude]);
+            if (pos == null) {
+                console.log(d);
+            }
+            return pos[0];
+        })
+        .attr('cy', function (d) {
+            var pos = projection([d.longitude, d.latitude]);
+            if (pos == null) {
+                console.log(d);
+            }
+            return pos[1];
+        })
+        .attr('r', '3.5')
+        .on('mouseover', showTooltip)
+        .on('mouseout', hideTooltip);
 
+
+    function showTooltip(d) {
+        var mouse = d3.mouse(mapSVG.node()).map(function (d) {
+            return parseInt(d);
+        });
+        hoverTooltip.classed('hidden', false)
+            .attr('style', 'left:' + (mouse[0] + 15) +
+                'px; top:' + (mouse[1] - 35) + 'px')
+            .html(d.name + "<br />");
+    }
+
+    function hideTooltip(d) {
+        hoverTooltip.classed('hidden', true);
     }
 
     scroll.on('active', function (index) {
